d6a40885e20a1e33a2260f99ccebe3377fb072d5
Chris Kirby <christian.h.kirby@gmail.com>
2021-05-25 15:45:55 -0800

    Added sockphone.js

--- commands/phones/sockphone.js
+++ commands/phones/sockphone.js
@@ -3,6 +3,6 @@ const bot = new Discord.Client();
 
 exports.run = function(message, args, bot) {
     //const command = args.shift().toLowerCase();
-    bot.channels.cache.get('842187257926516736').send("<@&842186819386605568"> +args);
+    bot.channels.cache.get('842187257926516736').send(`<@&842186819386605568> <@&842322700650676224>` +args);
 }
 
