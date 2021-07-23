c2ec1a2a5d7d03d0bb6176a75b627f0d547026b4
Chris Kirby <christian.h.kirby@gmail.com>
2021-07-23 14:34:14 -0400

    Updated guildBots.js

--- commands/utility/guildBots.js
+++ commands/utility/guildBots.js
@@ -11,32 +11,80 @@ exports.run = async (message, args, bot, db) => {
         message.reply( "No arguments specified");
         return;
     }
-
     try {
-        if (message.member.roles.cache.has(config.raiderRole)) {
-            if (params.hasOwnProperty('park') && params.park) {
-                await db.query("UPDATE meanBot.guildBots SET location = ?, notes = ?, updatedBy = ? WHERE name = ?;", [params.location, params.notes, message.author.id, params.name]);
-                message.reply(`Parked ${params.name} at ${params.location} with ${params.notes}`);
-            } else if (params.hasOwnProperty('location') && params.location) {
-                const rows = await db.query("SELECT gb.name, z.zoneName FROM meanBot.guildBots gb JOIN meanBot.zones z ON gb.location = z.zoneId;");
-                if (rows.length > 0) {
-                    console.log("$$$$$$$$$ Location: " + rows[0].zoneName);
-                    const embed = new Discord.MessageEmbed().setColor("#0099ff").setTitle("Current Bot Locations\n")
-                    .addFields(
-                        rows.map((row) => {
-                            return{
-                                name: `${row.name}`,
-                                value: `${row.zoneName}`
-                            }
-                        })
-                    ).setTimestamp();
+        const isLeadershipViewPasswords = params.hasOwnProperty('viewpasswords') && params.viewpasswords;
+        const isLeadershipUpdateNotes = params.hasOwnProperty("setnotes") && params.setnotes;
+        const isLeadershipUpdatePassword = params.hasOwnProperty("setpassword") && params.setpassword;
+        if (message.member.roles.cache.has(config.leadersRole) && isLeadershipViewPasswords) {
+               const rows = await db.query("SELECT name, username, password FROM meanBot.guildBots");
+               if (rows.length > 0) {
+                const embed = new Discord.MessageEmbed().setColor("#0099ff").setTitle("Current Passwords\n")
+                .addFields(
+                    rows.map((row) => {
+                        return {
+                            name: `${row.name}`,
+                            value: `Username: ${row.username}\nPassword: ${row.password}`
+                        }
+                    })
+                ).setTimestamp();
+                    message.author.send(embed);
+               }
+        } else if (message.member.roles.cache.has(config.leadersRole) && isLeadershipUpdateNotes) {
+            const rows = await db.query("UPDATE meanBot.guildBots SET notes = ? WHERE name = ?", [params.setnotes, params.name]);
+            message.author.send(`Updated notes for ${params.name} with ${params.setnotes}`);
+        } else if (message.member.roles.cache.has(config.leadersRole) && isLeadershipUpdatePassword) {
+            const rows = await db.query("UPDATE meanBot.guildBots SET password = ? WHERE name = ?", [params.setpassword, params.name]);
+            message.author.send(`Updated password for ${params.name} with ${params.setpassword}`);
+        }
+        else if (message.member.roles.cache.has(config.raiderRole) && !isLeadershipViewPasswords && !isLeadershipUpdateNotes && !isLeadershipUpdatePassword) {
+            const exists = await db.query("SELECT name FROM meanBot.guildBots WHERE name = ?", params.name);
+            if (exists == undefined || exists.length == 0) {
+                message.reply("This character does not exist");
+            } else {
+                if (params.hasOwnProperty('park') && params.park) {
+                    const zones = await db.query("SELECT z.zoneId FROM meanBot.zones z LEFT JOIN meanBot.zoneAliases za ON z.zoneId = za.zoneId WHERE za.zoneName = ?;", params.location);
+                    if (zones == undefined || zones.length == 0) {
+                        message.reply("Invalid zone specified!");
+                        return;
+                    }
+                    await db.query("UPDATE meanBot.guildBots SET location = ?, buffs = ?, updatedBy = ? WHERE name = ?;", [zones[0].zoneId, params.buffs, message.author.id, params.name]);
+                    message.reply(`Parked ${params.name} at ${params.location} with ${params.notes}`);
+                } else if (params.hasOwnProperty('location') && params.location) {
+                    const rows = await db.query("SELECT gb.name, z.zoneName FROM meanBot.guildBots gb JOIN meanBot.zones z ON gb.location = z.zoneId;");
+                    if (rows.length > 0) {
+                        const embed = new Discord.MessageEmbed().setColor("#0099ff").setTitle("Current Bot Locations\n")
+                        .addFields(
+                            rows.map((row) => {
+                                return{
+                                    name: `${row.name}`,
+                                    value: `${row.zoneName}`
+                                }
+                            })
+                        ).setTimestamp();
+                            message.author.send(embed);
+                    }
+                } else if (params.hasOwnProperty('name') && params.hasOwnProperty('showpassword')) {
+                    const rows = await db.query("SELECT name, username, password FROM meanBot.guildBots WHERE name = ?;", params.name);
+                    if (rows[0] !== null && rows[0] !== undefined) {
+                        const embed = new Discord.MessageEmbed().setColor("#0099ff").setTitle(`${rows[0].name}`).addFields(
+                            {name: `Username`, value: `${rows[0].username}`},
+                            {name: `Password`, value: `${rows[0].password}`}).setTimestamp();
                         message.author.send(embed);
+                    }
+                } else if (params.hasOwnProperty('name') && params.hasOwnProperty('showinfo')) {
+                    const rows = await db.query("SELECT gb.name, z.zoneName, gb.notes, gb.buffs FROM meanBot.guildBots gb JOIN meanBot.zones z ON gb.location = z.zoneId WHERE name = ?;", params.name);
+                    if (rows[0] !== null && rows[0] !== undefined) {
+                        const embed = new Discord.MessageEmbed().setColor("#0099ff").setTitle(`${rows[0].name}`).addFields(
+                            {name: `Location`, value: `${rows[0].zoneName}`},
+                            {name: `Buffs`, value: `${rows[0].buffs}`},
+                            {name: `Notes`, value: `${rows[0].notes}`}).setTimestamp();
+                        message.author.send(embed);
+                    }
                 }
             }
-        } else if (message.member.roles.cache.has(config.leaderRole)) {
-           
-        } else {
-            message.reply("you don't have the role");
+        }
+         else {
+            message.reply("You don't have the role");
         }
         
     } catch(ex) {
